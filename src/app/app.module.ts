d0f0a75e3bec19c43ac0f1950eaa4a74c5b060a9
Anthony Frénéat <anthonyfreneat@gmail.com>
2021-02-01 19:10:07 +0100

    dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductViewComponent } from './products/product-view/product-view.component';
-import { ProductUpdateComponent } from './products/product-update/product-update.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { ModalContentProductComponent } from './products/modal-content-product/modal-content-product.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     ProductViewComponent,
-    ProductUpdateComponent
+    ModalContentProductComponent
   ],
   imports: [
+    ReactiveFormsModule,
     CommonModule,
     BrowserModule,
     MatButtonModule,
