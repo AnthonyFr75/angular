bc8d1011f8df969de71ab2d70b867bb6934ce78b
Anthony Frénéat <anthonyfreneat@gmail.com>
2021-02-01 17:13:12 +0100

    init delete put + angular templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductViewComponent } from './products/product-view/product-view.component';
+import { ProductUpdateComponent } from './products/product-update/product-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductViewComponent,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule
